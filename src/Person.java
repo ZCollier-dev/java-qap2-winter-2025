3b3291b3716cc4a9a8150cf980cf015e2e98c2ac
Zachary Collier <zacharycollier36@protonmail.com>
2025-02-13 11:21:09 -0330

    Filled Person with constructor and toString methods
    - Another UML Diagram without a proper constructor. Takes a first name, last name, and an Address
    - toString returns a string in the format "Person: lastName, firstName home.toString"

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,17 @@
 public class Person {
+    //attributes
+    private String lastName;
+    private String firstName;
+    private Address home;
+
+    //methods
+    public Person(String firstName, String lastName, Address home){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.home = home;
+    }
+
+    public String toString(){
+        return String.format("Person: $s, %s %s", this.lastName, this.firstName, this.home.toString());
+    }
 }
