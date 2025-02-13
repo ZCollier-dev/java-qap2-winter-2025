33027d7f4c8ec2ff7f057fdcb7479661f7b95de6
Zachary Collier <zacharycollier36@protonmail.com>
2025-02-13 11:10:15 -0330

    Filled Address with constructor and toString methods
    - Why there was not a constructor method in the UML is beyond me
    - toString returns a string in the format "Address: street, city province postal"

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,19 @@
 public class Address {
+    //attributes
+    private String street;
+    private String city;
+    private String province; //replacing state because canada
+    private String postal; //replacing zip because canada
+
+    //methods
+    public Address(String street, String city, String province, String postal){
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.postal = postal;
+    }
+
+    public String toString(){
+        return String.format("Address: %s, %s %s %s", this.street, this.city, this.province, this.postal);
+    }
 }
